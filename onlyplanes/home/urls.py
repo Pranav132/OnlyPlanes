ae748b80c3b357873a9f94c26a65c0073566e86b
Pranav132 <mais.06pranav@gmail.com>
2022-01-08 15:48:32 +0530

    google oauth implented

--- onlyplanes/home/urls.py
+++ onlyplanes/home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 # django urls page to create routes
@@ -11,6 +12,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("home/", views.index, name="index"),
     path("index/", views.index, name="index"),
-    path("search", views.search)
-    
+    path("search", views.search),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # static media url and root to serve images uploaded through imagefield
--- onlyplanes/onlyplanes/settings.py
+++ onlyplanes/onlyplanes/settings.py
@@ -43,9 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     "bootstrap4",
     # adding apps
     'home',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +135,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+
+SITE_ID = 3
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
